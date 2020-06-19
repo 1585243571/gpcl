525758a44476db02e10f1cd918f0fbe9e30be474
zhangning <zhangning@momenta.ai>
2020-06-19 11:21:19 +0800

    update

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 '''
 @Author: your name
 @Date: 2020-05-23 23:54:22
-@LastEditTime: 2020-05-23 23:55:53
+@LastEditTime: 2020-05-24 09:30:05
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /y/股票策略/main.py
 '''
 import sys
 
+
 sys.path.append("./tools")
 
+import baostock as bs
+import pandas as pd
+from pathlib import Path
+import os
 import gupiao_fenlei
+import celuoe
+
 
 if __name__ == "__main__":
     gupiao_fenlei.jj_fl()
+    lg = bs.login()
+    celuoe.test1(bs)
--- tools/celuoe.py
+++ tools/celuoe.py
@@ -1,23 +1,24 @@
 '''
 @Author: your name
 @Date: 2020-05-24 08:55:16
-@LastEditTime: 2020-05-24 12:01:46
+@LastEditTime: 2020-05-25 23:29:19
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /y/股票策略/tools/celuoe.py
 '''
 import baostock as bs
 import pandas as pd
-
+import os
 
 def test1(bs):
     rs = bs.query_stock_industry()
     print(rs.get_row_data())
     
 class Interface:
-    def __init__(self,bs,start_date='2020-04-13',end_date='2020-04-14'):
+    def __init__(self,bs,start_date='2020-06-15',end_date='2020-06-16'):
         #### 登陆系统 ####
-        self.bs_ = bs
+        self.bs_ = bs 
+        self.tatol_=0
         lg = self.bs_.login()
         # 显示登陆返回信息
         print('login respond error_code:'+lg.error_code)
@@ -26,14 +27,24 @@ class Interface:
         self.hsitory_k_data_ = []
         for val in self.industry_list_:
             data = self.get_history_k_data(val[1],start_date,end_date)
-            if len(data) > 0:
+#            print(data)
+            if len(data) == 2:
                 self.hsitory_k_data_.append(data) 
-                #print(data2)
+ #               print(data[1])
                 self.strategy(data[0],data[1])
+            elif len(data) == 3:
+                self.strategy_3(data[0],data[1],data[2])
+            elif len(data) == 1:
+                self.strategy_4(data[0])
         print("hstory_k_data_size:%d",len(self.hsitory_k_data_))
+        print("----------------:%d",self.tatol_) 
+        self.stop()
     def stop(self):
         ### 关闭系统 ###
         self.bs_.logout()
+    def strategy_er_zhu(self,data1,data2):
+        pass 
+
     def strategy(self,data1,data2):
         #策略一  open 2 ，high 3 , low 4,close 5 找锤子线看涨
             bz = 10
@@ -46,9 +57,26 @@ class Interface:
                         if  ((float(data2[2]) - float(data2[4])) / v ) > bz:
                             if (float(data2[3]) - float(data2[5])) / v < 10:
                                 print(data2)
+                                cmd = "grep -r  " + data2[1] + " /home/zn/y/股票策略/file "
+                                os.system(cmd)
 
-                        
+    def strategy_3(self,data,data1,data2):
+        if float(data[2]) < float(data[5]):
+            if float(data1[2]) < float(data[5]):
+                if float(data2[2]) < float(data1[5]):
+                    print(data2)
+                     
 
+    def strategy_4(self,data2):
+        bz =10
+        v = (float(data2[5]) - float(data2[2]))
+        if v == 0:
+            v = 0.001
+        if  ((float(data2[2]) - float(data2[4])) / v ) > bz:
+            if (float(data2[3]) - float(data2[5])) / v < 10:
+                print(data2)
+                cmd = "grep -r  " + data2[1] + " /home/zn/y/股票策略/file "
+                os.system(cmd)
 
         
     def get_industry_list(self):
@@ -79,5 +107,11 @@ class Interface:
 
 if __name__ == "__main__":
     test = Interface(bs=bs)
-    test.strategy()
-    
+    '''for i in range(10):
+        if i == 0:
+            continue
+        a = '2020-05-'+ str(i+5);
+        print(a)
+        test = Interface(bs,a,a)
+        
+    '''
